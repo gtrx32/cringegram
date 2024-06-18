f130274306422201c6aad1ffb18333d9fdf81bdf
gtrx32 <gtrx32@bk.ru>
2024-06-18 15:44:22 +0300

    search toggle

--- src/components/Layout/Header/SearchBlock/index.tsx
+++ src/components/Layout/Header/SearchBlock/index.tsx
@@ -1,26 +1,41 @@
 import s from "./SearchBlock.module.scss";
-import { useSearchUsersQuery, usersApi } from "@/api/usersApi";
+import { useSearchUsersQuery } from "@/api/usersApi";
 import Input from "@/components/Input";
-import { useState } from "react";
+import { useClickOutside } from "@react-hooks-library/core";
+import { useEffect, useRef, useState } from "react";
 
 const SearchBlock = () => {
   const [value, setValue] = useState("");
-  const { data } = useSearchUsersQuery(value, { skip: value.length < 1 });
   const onChange = (value: string) => setValue(value);
+  const { data } = useSearchUsersQuery(value, { skip: value.length < 1 });
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (data) setIsOpen(true);
+  }, [data]);
+
+  const ref = useRef(null);
+
+  useClickOutside(ref, () => {
+    setIsOpen(false);
+  });
 
   return (
     <div className={s.search_container}>
       <Input onChange={onChange} placeholder="Поиск людей по имени" />
-      <div className={s.search_results}>
-        {data?.map((user) => (
-          <div key={user.id} className={s.result_item}>
-            <div>
-              <p className={s.username}>{user.username}</p>
-              <p className={s.name}>{user.name}</p>
+      {isOpen && (
+        <div ref={ref} className={s.search_results}>
+          {data?.map((user) => (
+            <div key={user.id} className={s.result_item}>
+              <div>
+                <p className={s.username}>{user.username}</p>
+                <p className={s.name}>{user.name}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
